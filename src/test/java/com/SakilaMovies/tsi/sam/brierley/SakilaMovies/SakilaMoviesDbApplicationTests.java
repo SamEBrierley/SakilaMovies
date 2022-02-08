4517343827346dbd3bd4dca0b404f08c0b4bf88c
SamEBrierley <samellisbrierley@gmail.com>
2022-02-08 15:03:14 +0000

    Setting up sonar cloud

---
+++ src/test/java/com/SakilaMovies/tsi/sam/brierley/SakilaMovies/SakilaMoviesDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SakilaMovies.tsi.sam.brierley.SakilaMovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaMoviesDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
